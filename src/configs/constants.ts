d29a0305b34cc050131c4a9d5e862f4e07001db6
ntrungkieen <ninhtrungkien2811@gmail.com>
2025-02-17 09:21:19 +0700

    chore: update logo and project name

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,3 +1,3 @@
-export const PROJECT_NAME = 'Ollama PWA';
+export const PROJECT_NAME = 'Ollamapwa';
 export const DEFAULT_OLLAMA_HOST = 'http://127.0.0.1:11434';
 export const PREFERENCES_KEY = 'ollama-pwa:preferences';
